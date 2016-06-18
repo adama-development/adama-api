9fd76dd8ee3931d3d1fe7f9c114c12bda6b961ec
Romain Gonord <romain.gonord.opensource@neteyes.org>
2016-06-18 23:46:33 +0800

    add adama-swagger

---
+++ adama-swagger/src/main/java/com/adama/api/swagger/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.adama.api.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    // equivalents for <mvc:resources/> tags
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:META-INF/swagger-ui/");
+    }
+
+    // equivalent for <mvc:default-servlet-handler/> tag
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
